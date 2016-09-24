2064a26b7ccd77099c4704c24a3466f83340db20
Richard Hull <rm_hull@yahoo.co.uk>
2016-09-24 19:56:41 +0100

    Update project config

--- project.clj
+++ project.clj
@@ -1,39 +1,25 @@
-(defproject rm-hull/weasel "0.0.1-SNAPSHOT"
-  :clojurescript? true
-  :description "A Clojure/Clojurescript weasel"
-  :source-paths ["src"]
+(defproject rm-hull/weasel "0.1.0"
+  :description "Demonstration of the process that drives evolutionary systems"
   :url "https://github.com/rm-hull/weasel"
-  :license {:name "The MIT License (MIT)"
-            :url "http://opensource.org/licenses/MIT"}
-  :scm {:url "git@github.com:rm-hull/weasel"}
-  :min-lein-version "2.3.4"
-  :global-vars {*warn-on-reflection* true}
-  :plugins [[lein-cljsbuild "1.0.3"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2322"]
-                 [rm-hull/dommy "0.1.3-SNAPSHOT"]
-                 [rm-hull/cljs-test "0.0.8-SNAPSHOT"]
-                 [rm-hull/big-bang "0.0.1-SNAPSHOT"]
-                 [rm-hull/cljs-dataview "0.0.1-SNAPSHOT"]
-                 [rm-hull/wireframes "0.0.1-SNAPSHOT"]]
-  :cljsbuild {
-    :test-commands {"phantomjs" ["phantomjs" "target/unit-test.js"]}
-    :builds {
-      :test {
-        :source-paths ["src" "test"]
-        :incremental? true
-        :compiler {
-          :output-to "target/unit-test.js"
-          :source-map "target/unit-test.map"
-          :static-fns true
-          :optimizations :whitespace
-          :pretty-print true }}
-      :examples {
-        :source-paths ["src" "examples"]
-        :incremental? true
-        :compiler {
-          :output-to "target/example.js"
-          :source-map "target/example.map"
-          :static-fns true
-          ;:optimizations :advanced
-          :pretty-print true }}}})
+  :license {
+    :name "The MIT License (MIT)"
+    :url "http://opensource.org/licenses/MIT"}
+  :dependencies []
+  :scm {:url "git@github.com:rm-hull/weasel.git"}
+  :vcs :git
+  :source-paths ["src"]
+  :jar-exclusions [#"(?:^|/).git"]
+  :codox {
+    :source-paths ["src"]
+    :output-path "doc/api"
+    :source-uri "http://github.com/rm-hull/weasel/blob/master/{filepath}#L{line}"  }
+  :min-lein-version "2.6.1"
+  :profiles {
+    :dev {
+      :global-vars {*warn-on-reflection* true}
+      :plugins [
+        [lein-codox "0.9.5"]
+        [lein-cloverage "1.0.6"]]
+      :dependencies [
+        [org.clojure/clojure "1.8.0"]]}})
+
