4405f40fbca4a9190d8eb4f7733bd0c08afdb9af
Richard Hull <rm_hull@yahoo.co.uk>
2016-09-25 19:44:09 +0100

    Move make functions to start; Collapse max-x/y into dimensions array

--- src/weasel/mona_lisa/gene_builder.clj
+++ src/weasel/mona_lisa/gene_builder.clj
@@ -5,6 +5,30 @@
   (rand-int 256))
 
 
+;; gene creation
+
+(defn- make-color []
+  (list
+    (rand-color)   ; red
+    (rand-color)   ; green
+    (rand-color)   ; blue
+    (rand-color))) ; alpha
+
+(defn- make-vertex [[max-width max-height :as dimensions]]
+  (fn []
+    [(rand-int max-width)
+     (rand-int max-height)]))
+
+(defn- make-polygon [dimensions]
+  (let [rand-vertex (make-vertex dimensions)]
+    (fn []
+      {:color (make-color)
+       :points (repeatedly (+ 3 (rand-int 3)) rand-vertex)})))
+
+(defn make-dna [dimensions num-genes]
+  (repeatedly num-genes (make-polygon dimensions)))
+
+
 ;; gene mutation
 
 (defn- mutate [lst probability rand-fn]
@@ -26,8 +50,8 @@
       (concat (take n lst) (drop (inc n) lst)))
     lst))
 
-(defn- mutate-polygon [max-x max-y]
-  (let [rand-vertex (make-vertex max-x max-y)]
+(defn- mutate-polygon [dimensions]
+  (let [rand-vertex (make-vertex dimensions)]
     (fn [polygon probability]
       (->
         polygon
@@ -36,10 +60,14 @@
         (update :points insert probability rand-vertex)
         (update :points remove probability)))))
 
-(defn mutate-dna [max-x max-y dna probability]
-  (let [mutate (mutate-polygon max-x max-y)]
-    (for [gene dna]
-      (mutate gene probability))))
+(defn mutate-dna [dimensions probability]
+  (let [mutate (mutate-polygon dimensions)]
+    (fn [dna]
+      (for [gene dna]
+        (mutate gene probability)))))
+
+
+;; gene uniform crossover
 
 (defn crossover-dna [dna1 dna2]
   (for [[gene1 gene2] (map list dna1 dna2)]
@@ -48,25 +76,3 @@
       gene2)))
 
 
-;; gene creation
-
-(defn- make-color []
-  (list
-    (rand-color)   ; red
-    (rand-color)   ; green
-    (rand-color)   ; blue
-    (rand-color))) ; alpha
-
-(defn- make-vertex [max-x max-y]
-  (fn []
-    [(rand-int max-x)
-     (rand-int max-y)]))
-
-(defn- make-polygon [max-x max-y]
-  (let [rand-vertex (make-vertex max-x max-y)]
-    (fn []
-      {:color (make-color)
-       :points (repeatedly (+ 3 (rand-int 3)) rand-vertex)})))
-
-(defn make-dna [num-genes max-x max-y]
-  (repeatedly num-genes (make-polygon max-x max-y)))
